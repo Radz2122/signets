e4679743538b8a0ae6a8c6a27b19a9f40132d279
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2021-03-18 17:18:03 -0400

    version prof

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,13 @@ import * as firebaseui from 'firebaseui';
 import 'firebase/firestore';
 
 // Configuration
-const firebaseConfig = {
-  
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQAztvvcvke-wpmASPjs1jVWuqUFL3gdQ",
+    authDomain: "signets-b5ec7.firebaseapp.com",
+    projectId: "signets-b5ec7",
+    storageBucket: "signets-b5ec7.appspot.com",
+    messagingSenderId: "835294749593",
+    appId: "1:835294749593:web:5dab593e7a65c7e6a8a908"
 };
 
 // Initialiser Firebase
